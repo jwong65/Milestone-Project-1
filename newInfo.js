e71fa1a2541cf377c77b1f56fdd246f4c7d28dd6
jwong65 <jason.wong6507@gmail.com>
2022-11-16 19:27:07 -0500

    Moved the displayInformation into another file

--- newInfo.js
+++ newInfo.js
@@ -1,1 +1,9 @@
 //This will be the file that is being used to give information to the player from the beginning.
+//Need a function call for information everything something changes.
+
+function displayInformation(input){
+    let textbox = input
+    document.getElementById('info').innerHTML = textbox;
+}
+
+// displayInformation("This is now changing.")
