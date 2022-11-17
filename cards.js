caee3bdf95db8f14fb70173385f273eea415fff4
jwong65 <jason.wong6507@gmail.com>
2022-11-16 21:26:02 -0500

    Created the cards.js file and added a placeholder button to show deck

---
+++ cards.js
@@ -0,0 +1,1 @@
+//I want to use another file to store the cards.
--- newHand.js
+++ newHand.js
@@ -55,7 +55,7 @@ function newCard(deck){
     //getRndInteger(0,0)
     let draw = deck[0]
     let newImage = document.createElement('img')
-    newImage.src = "assets/SM35_EN_45.png"
+    //newImage.src = "assets/SM35_EN_45.png"
     document.getElementById('hand').appendChild(newImage)
     displayInformation(draw.text, "info")
 
