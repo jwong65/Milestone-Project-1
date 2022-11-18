d6f51f1a7b8689ae364d82737b7ef1f10bb3fcee
jwong65 <jason.wong6507@gmail.com>
2022-11-18 18:23:53 -0500

    Testing card1 funtionality

--- cardsFunct.js
+++ cardsFunct.js
@@ -1,4 +1,7 @@
 //I want to use another file to store the cards.
-function card1Function(){
-    
+function card1Function(card1){
+    console.log(Number(card1.value) +(playerhealth))
+    // if (card1.value +playerhealth)
 }
+
+card1Function(card1)
