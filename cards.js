23a358c8b4a33775b581d1999510bce7752269ad
jwong65 <jason.wong6507@gmail.com>
2022-11-19 01:51:30 -0500

    Edited the rule text to allow for more clarity and generated the first 5 cards.

--- cards.js
+++ cards.js
@@ -10,6 +10,10 @@ function cardCreation(type, value, text){
     this.value =value
     this.text = text
 }
-const card1 = new cardCreation('heal', '5', 'This card will heal you for 5 health')
+const card1 = new cardCreation('heal', 5, 'This card will heal you for 5 health')
 // This is now the way to create cards instead of using the const creating an object
-const card2 = new cardCreation()
+const card2 = new cardCreation('damage', 2, "This card does 2 damage to your opponent")
+const card3 = new cardCreation('block', 1, "The next attack you take will be reduced by 1.")
+const card4 = new cardCreation('recycle', null, "All cards in the discard pile are shuffled back into the deck. This card cannot be discarded.")
+const card6 = new cardCreation('draw', 1 , "You can draw one additional card this turn. You cannot attack if you play this card")
+//Instead of creating new cardCreation of the damage 2 card, I will push additional copies of the card to generate the deck.
