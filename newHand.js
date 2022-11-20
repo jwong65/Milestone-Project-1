31ee548ffba6479706c92f9b42f745260590637d
jwong65 <jason.wong6507@gmail.com>
2022-11-20 05:12:26 -0500

    Adding the new file broke the regular hand function because I could not see if I set the attribute to opphand

--- newHand.js
+++ newHand.js
@@ -75,6 +75,23 @@ function newHand(){
     return hand
 }
 
+function oppHand(){
+    let oppHand = document.createElement('div')
+    oppHand.style.width ='50%'
+    oppHand.style.height ='100px'
+    oppHand.style.display='flex'
+    oppHand.style.flexDirection='row'
+    oppHand.style.justifyContent = 'space-evenly'
+    oppHand.setAttribute = ('id', 'opphand')
+    oppHand.style.border='1px solid black'
+    oppHand.style.backgroundColor ='orange'
+    document.body.append(oppHand)
+    oppHand.style.position='relative'
+    oppHand.style.left ="550px"
+    oppHand.style.bottom = "500px"
+}
+
+const drawOpp = oppHand()
 const drawHand = newHand()
 //append only works if there's an Node object/String object being passed?
 
