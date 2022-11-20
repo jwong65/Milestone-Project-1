4f15e6648fa31b0c4570a79b7c6b2f0479ecba3a
jwong65 <jason.wong6507@gmail.com>
2022-11-20 03:32:06 -0500

    Completed functionality of card3 and card4, not tested yet

--- cardsFunct.js
+++ cardsFunct.js
@@ -25,7 +25,22 @@ function card2Function(card2, directedHealth, target){
 // card2Function(card2, enemyhealth, "opphealth")
 // console.log(enemyhealth)
 
-//Card 3 function to do damage
+//Card 3 function to reduce damage
 function card3Function(card3, target){
-    
+    //target can be turnDefense or oppDefense
+    if (target =="player"){
+        turnDefense+=card3.value
+    }
+    else{
+        opponentDefense+=card3.value
+    }
+}
+
+//Card 4 function to recyle the discard pile. To make it simple the opponent will never run out of cards so they will not play this card.
+function card4Function(){
+    discardpile.forEach(()=>{
+        returnCard= discardpile.pop()
+        deck.push(returnCard)
+        shuffleDeck()
+    })
 }
