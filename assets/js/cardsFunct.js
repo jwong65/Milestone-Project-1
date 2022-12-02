c8b504c54972d646fab41b554457c6529e64306b
jwong65 <jason.wong6507@gmail.com>
2022-12-01 20:13:00 -0500

    Removed the extra comments on cardFunction that were previously planned cards

--- assets/js/cardsFunct.js
+++ assets/js/cardsFunct.js
@@ -13,32 +13,13 @@ function card1Function(card1, target){
         console.log("Card1 activated")
     }
 }
-// card1Function(card1, playerhealth, "HP")
-// console.log(playerhealth)
-//An example of the card1 being use - card1Function(card1, playerhealth, "HP")
-
 //Card 2 function to do damage.
 function card2Function(card2, target){
     enemyhealth = enemyhealth - card2.value
     displayHealth(enemyhealth, target)
 }
-// card2Function(card2, enemyhealth, "opphealth")
-// console.log(enemyhealth)
-
 //Card 3 function to reduce damage
-//This function could be used against the player if I have time post-MVP
-//I am changing this function because I feel the game could drag on if any type of defense was applied, this will be multiplicative to cardsPlayed
 function card3Function(target){
-    //target can be turnDefense or oppDefense
-    // if (target =="player"){
-    //     turnDefense+=card3.value
-    // }
-    // else{
-    //     opponentDefense+=card3.value
-    // }
-    
-    // console.log(numberofCardplayed + " Number of Cards played")
-    // console.log(numberofCardplayed*3 + "Times 3 = damage")
     enemyhealth = enemyhealth - (numberofCardplayed*3)
     displayHealth(enemyhealth, target)
 }
