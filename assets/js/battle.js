979fc4096bb5d23106f6000efac1fd12b175115a
jwong65 <jason.wong6507@gmail.com>
2022-12-01 20:16:05 -0500

    Removed the commented out section for resetTurn, was not used

--- assets/js/battle.js
+++ assets/js/battle.js
@@ -31,15 +31,6 @@ function checkManyPlayed(){
     }
 }
 
-
-// //A function will be needed to reset these after every turn.
-// function resetTurn(numberofCardplayed){
-//     //turnDefense, damageAllowed
-//     numberofCardplayed = 0;
-//     // damageAllowed =true;
-// }
-//Example call would be resetTurn(numberofCardplayed, turnDefense, damageAllowed) This should be called when endTurn is called, but because endTurn is in index and it is above the declaration of battle.js it may need to be moved.
-
 //Opponent turn will have to be put on a setTimeout() that will be okay enough that it doesn't seem jarring.
 //A button will be needed to show the log of what the opponent has done.
 
